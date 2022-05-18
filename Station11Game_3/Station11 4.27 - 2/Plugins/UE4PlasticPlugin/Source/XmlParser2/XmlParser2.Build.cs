9f989892995ff3f2073fe59c9f4318ca0b3d0261
mccomdw1 <douglas.mccomish@gmail.com>
2022-05-18 15:31:51 +1200

    design school content merged into BIT project

---
+++ Station11Game_3/Station11 4.27 - 2/Plugins/UE4PlasticPlugin/Source/XmlParser2/XmlParser2.Build.cs
@@ -0,0 +1,19 @@
+﻿// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class XmlParser2 : ModuleRules
+{
+	public XmlParser2(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				"CoreUObject",
+			}
+		);
+	}
+}
